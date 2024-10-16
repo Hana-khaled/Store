2b6cd13b17d27ebf15756ad5aafaff3b12a3be76
Hana Khaled <Hana_khaled99@outlook.com>
2024-10-17 01:40:22 +0200

    Applying AutoMapper

---
+++ Store.Services/Mapping/ProductProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Store.Data.Entities;
+using Store.Services.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Services.Mapping
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.BrandName, option => option.MapFrom(src => src.Brand.Name))
+                .ForMember(dest => dest.TypeName, option => option.MapFrom(src => src.Type.Name));
+        }
+    }
+}
