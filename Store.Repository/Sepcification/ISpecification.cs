b4eda07e1315cf9ecdbb0607ff5c745479b33055
Hana Khaled <Hana_khaled99@outlook.com>
2024-10-17 10:34:19 +0200

    Apply Specification and Base Specification

---
+++ Store.Repository/Sepcification/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Sepcification
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; } // for where conditions
+        List<Expression<Func<T, object>>> Includes { get; }
+        // different types of includes (Brands - Types)
+    }
+}
